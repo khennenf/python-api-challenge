0139084c040aba4035a34eed595805d69037b0cb
Keri <keri@rogue.internal>
2021-02-05 21:34:19 -0500

    changes final

--- Homework/Instructions/starter_code/api_keys.py
+++ Homework/Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # # OpenWeatherMap API Key
-# weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "NEED KEY HERE"
 
 # Google API Key
-g_key = "AIzaSyD67hPNU_LSlCM-BpPNko41_ydg7SL421U"
+g_key = "NEED KEY HERE"
